4925fe2a7af8bc3e7c96cfe4bbb4896af6f0d6a6
Tobias Löb <g3neration0@gmail.com>
2013-11-20 17:20:49 +0100

    added local temp.db

--- site/assets/py/fetchdata.py
+++ site/assets/py/fetchdata.py
@@ -3,8 +3,17 @@
 import time
 import pywapi
 import pprint
+import sqlite3
+
 pp = pprint.PrettyPrinter(indent=4)
 
+#Verbinde zu Datenbank
+connection = sqlite3.connect('temps.db')
+cursor = connection.cursor()
+
+#Drop all existing Tables
+cursor.execute("DROP TABLE Temperatures")
+	
 def getData():
 	cities = pywapi.get_cities_from_google('de') # or (country = 'fr', hl = 'de')
 	locidhn = pywapi.get_loc_id_from_weather_com("Hannover")
@@ -27,8 +36,18 @@ def getData():
 	pp.pprint("Aktuelle Temperatur in London: " + templo['current_conditions']['temperature'] +" Grad")
 	pp.pprint("Aktuelle Temperatur in Hannover: " + temphn['current_conditions']['temperature'] +" Grad")
 	
+	cursor.execute(" INSERT INTO temp VALUES ('Braunschweig',?, ?)", datetime, tempbs)
+
+	
+cursor.execute(" CREATE TABLE Temperatures(origin text, time text, temp text)")
 	
 for i in range(0, 10):
+	
 	pp.pprint("+++++++++++++++++++++++++++++++++++")
 	getData()
-	time.sleep(1)
+	writeData()
+	cursor.execute(" SELECT * FROM Temperatures")
+	time.sleep(3)
+
+connection.commit()
+connection.close()
